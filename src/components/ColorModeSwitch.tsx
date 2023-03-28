44bdf62d479ac0f8e8b6346cc57baf3c7c9d87ff
Ifeanyi Umeh <ifeanyi4umeh@gmail.com>
2023-03-28 19:06:25 +0100

    updated color mode icons

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,14 +1,24 @@
-import { HStack, Switch, Text, useColorMode } from '@chakra-ui/react'
+import { HStack, Switch, Text, useColorMode } from '@chakra-ui/react';
+import { MdLightMode } from 'react-icons/md';
+import { MdOutlineLightMode } from 'react-icons/md';
 
 const ColorModeSwitch = () => {
-  const {toggleColorMode, colorMode} = useColorMode();
+  const { toggleColorMode, colorMode } = useColorMode();
 
   return (
     <HStack>
-      <Switch colorScheme='green' isChecked={colorMode === 'dark'} onChange={toggleColorMode} />
-      <Text whiteSpace='nowrap'>Dark Mode</Text>
+      <Switch
+        colorScheme="green"
+        isChecked={colorMode === 'dark'}
+        onChange={toggleColorMode}
+      />
+      {colorMode === 'light' ? (
+        <MdLightMode size={25} />
+      ) : (
+        <MdOutlineLightMode size={25} />
+      )}
     </HStack>
-  )
-}
+  );
+};
 
-export default ColorModeSwitch
+export default ColorModeSwitch;
